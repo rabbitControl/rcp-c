1a66a588e52ee8011c8e2be18420130cf061dc10
Ingo Randolf <ingo.randolf@servus.at>
2024-02-26 15:16:53 +0800

    update license header

--- rcp_slip.c
+++ rcp_slip.c
@@ -7,7 +7,7 @@
 *
 * This file is part of rabbitcontrol for c.
 *
-* Written by Ingo Randolf, 2021 - 2022
+* Written by Ingo Randolf, 2021 - 2024
 *
 * This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
